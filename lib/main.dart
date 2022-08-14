d536c906f55139d68ac5f91b7cdde4aace45a54b
Blanka Semanová <bsemanova@deloittece.com>
2022-08-14 10:36:44 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey[200],
+          appBar: AppBar(
+            title: Text('Ask me anything'),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: MagicBall(),
+        ),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int randomAnswer = 1;
+
+  void getRandomAswer (){
+    setState((){
+      randomAnswer = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: ((){
+          getRandomAswer();
+        }),
+        child: Image(
+          image: AssetImage('images/ball$randomAnswer.png'),
+        ),
+      ),
+    );
+  }
+}
